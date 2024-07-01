c8eb8003eb4ccf94202042a53d357d652645f826
bhupendrasambare <bhupendrasam1404@gmail.com>
2024-07-01 23:10:15 +0530

    initial comment

---
+++ src/main/frontend/views/@index.tsx
@@ -0,0 +1,7 @@
+export default function home(){
+    return(
+        <div>
+            <h1>Hello Bhupendra sambare</h1>
+        </div>
+    )
+}
---
+++ src/test/java/com/service/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
